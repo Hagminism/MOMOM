1f693b44621a0d2558d9d2e32eb7307fc27a0f25
Hagmin.Lee <iilhm2001@naver.com>
2025-12-07 17:40:20 +0900

    feat: `Firebase.initializeApp` 호출 시 실행된 플랫폼에 맞게 초기화를 진행할 수 있게 `DefaultFirebaseOptions.currentPlatform`를 사용하도록 수정

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,11 @@ import 'package:newflutter/main_page.dart';
 import 'login_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
 
